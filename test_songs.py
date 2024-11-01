935641ed8341e792fda1317d82afff81f91e87d5
Patrick Kubiak <epicpatka@gmail.com>
2024-10-31 20:19:01 -0400

    Update test_songs.py

--- test_songs.py
+++ test_songs.py
@@ -89,46 +89,6 @@ def test_process_song_exists(
     )
 
 
-@patch(
-    "fetch_lyrics.FetchLyrics.FetchLyrics.fetch_lyrics", return_value=mockedFetchLyrics
-)
-@patch(
-    "translate_lyrics.TranslateLyrics.TranslateLyrics.translate_lyrics",
-    return_value="english translation",
-)
-@patch("display_lyrics.ConsoleDisplayLyrics.ConsoleDisplayLyrics.display_lyrics")
-@patch("songs.SongDatabaseHandler", MockSongDatabaseHandler)
-def test_process_song_exists(
-    mocked_display_lyrics, mocked_translate_lyrics, mocked_fetch_lyrics
-):
-    """
-    Song already exists, fetch from database, and display original and English side-by-side lyrics.
-    """
-
-    # Prepare
-    song = "test_song_name"
-    artist = "test_artist_name"
-    access_keys = {
-        "GENIUS_ACCESS_TOKEN": "fake_token_1",
-        "MS_TRANSLATOR_KEY": "fake_token_2",
-        "MS_TRANSLATOR_REGION": "fake_token_3",
-    }
-    experimental = False
-
-    # Act
-    process_song(song, artist, access_keys, experimental)
-
-    # Assert
-    # song searched by name and artist
-    mocked_fetch_lyrics.assert_called_with(song, artist)
-    # mocked lyrics get translated
-    mocked_translate_lyrics.assert_called_with("mocked_lyrics")
-    # display is called with song info, original and translated lyrics
-    mocked_display_lyrics.assert_called_with(
-        ANY, "mocked_lyrics", "english translation"
-    )
-
-
 @patch("fetch_lyrics.FetchLyrics.FetchLyrics.fetch_lyrics", return_value=None)
 @patch(
     "translate_lyrics.TranslateLyrics.TranslateLyrics.translate_lyrics",
